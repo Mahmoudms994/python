2372e7a9d0477422b4da4d1d53e6799e571ba170
Mahmoudms994 <mahmoudsaifo5@gmail.com>
2021-10-18 13:03:06 +0300

    python new

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print("hello world")
+# num = int(input("enter number: "))
+# if num > 0:
+#     print("number is positive")
+# elif num <0:
+#     print("numer is nigative")
+# else:
+#     print ("nuber is zero")
+# name = input("enter ur name")
+# print (f"hello {name}, ur da bst")
+# frinds =["mahmoud" , "mahmoud" , " aLi" , "smd" , "zyt" ]
+# frinds.sort()
+# frinds.append("riky")
+# frinds.sort()
+# print(frinds)
+# # frinds = {"key01":"smd", "key02":"zyt"}
+# for i in frinds:
+#     print (i)
+# def name() {}
+num1 = int(input("enterur hight:"))
+num2 = int(input("enterur wight: "))
+if num1<100
+print(num2/(num1*num1))
